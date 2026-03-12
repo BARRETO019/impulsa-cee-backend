82765737684c5f136fe55c8f20fc1e7570ba8f45
BARRETO019 <emiliobarreto307@gmail.com>
2026-03-12 11:24:22 +0100

    fix airtable service export

--- hash.js
+++ hash.js
@@ -2,7 +2,7 @@
 const bcrypt = require('bcryptjs');
 
 async function generar() {
-  const hash = await bcrypt.hash("Tecnico2025", 10);
+  const hash = await bcrypt.hash("", 10);
   console.log(hash);
 }
 
